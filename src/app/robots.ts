04d61d2b0685feccdb7935037b849ddade6a0fe0
otto-camp <ismailyarar4@gmail.com>
2024-02-27 19:27:43 +0300

    Update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,14 @@
-export default function robots() {
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/*/_next/*", "https://analytics.yarar.dev/*"],
+      },
+    ],
     sitemap: "https://yarar.dev/sitemap.xml",
   };
 }
